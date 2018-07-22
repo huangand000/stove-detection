5d8d36b5a528a470321adeeeb4007bd04772f155
Liam Sexton <liam@Liams-MacBook-Pro.local>
2018-07-21 17:16:13 -0700

    Added comments and minor fixes

--- SquelchAlgorithm.py
+++ SquelchAlgorithm.py
@@ -3,21 +3,21 @@ import datetime
 
 class SquelchAlgorithm:
 
+    # TODO: How often to update/how long to keep track of moving averages of stove duration and max temp
 
-    # assuming one data point (a temperature) taken every minute during interval length
     def __init__(self, interval_len, delta):
-        self.recent_temps = MovingAverage(interval_len)
-        self.state = "OFF"
-        self.delta = delta
-        self.times_on = []
-        self.stove_duration = MovingAverage(30)
-        self.max_temp_today = 0
-        self.max_temp = MovingAverage(30)
-        self.prev_temp = None
+        self.recent_temps = MovingAverage(interval_len) # A moving average of the stove temperature (while off)
+        self.state = "OFF" # State of the stove (OFF, ON, or SIMMERING)
+        self.delta = delta # Minimum difference for a sudden large change in temp
+        self.times_on = [] # A list containing the periods of time that the stove has been on recently
+        self.stove_duration = MovingAverage(30) # Moving average of the time the stove is on
+        self.max_temp_today = 0 # The max temperature of the stove today
+        self.max_temp = MovingAverage(30) # Moving average of the max temp of the stove
+        self.prev_temp = None # The previously recorded temp of the stove
 
-
+    # Updates the state of the stove, and the moving average if the stove is off
     def update_state(self, curr_temp):
-        self._initial_data_gathering(curr_temp)
+        # self._initial_data_gathering(curr_temp)
         prev_state = self.state
         if curr_temp > self.threshold and curr_temp > self.delta + self.moving_average:  # if hot and large increase in temp
             self.state = 'ON'
@@ -30,25 +30,30 @@ class SquelchAlgorithm:
         else:
             self.state = 'OFF'
             self.curr_states["OFF"] = str(datetime.datetime.now()).split('.')[0]
-
         if self.state == 'OFF':  # Moving average only updates if the stove is off
             self.recent_temps.update_average
         self._check_and_stove_duration(prev_state, curr_temp)
 
-    # how often should this be called
+
+    # Should only be updated if the stove has been turned on today (if max_temp_today > some threshold temp)
+    # Updates the moving average of the max temp of the stove and resets max_temp_today to 0
     def update_max_temp_moving_average(self):
-        self.max_temp.update_average()
+        self.max_temp.update_average(self.max_temp_today)
         self.max_temp_today = 0
 
-    def _check_and_stove_duration(self, curr_state, new_temp):
+
+    # Checks to see if the stove was on and is now off, and updates the moving average of the stove duration.
+    def _check_and_stove_duration(self, curr_state, curr_temp):
         if (curr_state == "ON" and self.state == "OFF") or (curr_state == "SIMMERING" and self.state == "OFF"):
             if ("ON" in self.curr_states or "SIMMERING" in self.curr_states) and "OFF" in self.curr_states:
                 on_time = datetime.datetime.strptime(self.curr_states["ON"], "%Y-%m-%d %H:%M:%S")
                 off_time = datetime.datetime.strptime(self.curr_states["OFF"], "%Y-%m-%d %H:%M:%S")
-                self.times_on.append(on_time - off_time)
-        self.prev_temp = new_temp
+                self.stove_duration.update_average(on_time - off_time)
+        self.prev_temp = curr_temp
 
 
-    def _initial_data_gathering(self, new_temp):
-        if len(self.data_points) < self.interval_len:
-            self.data_points.append(new_temp)
+    # This method may not be necessary because of the moving average class.
+    # Adds the current temp to the data
+    # def _initial_data_gathering(self, curr_temp):
+        # if len(self.data_points) < self.interval_len:
+            # self.data_points.append(curr_temp)

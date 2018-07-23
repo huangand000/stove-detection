4cdbf14c1f562238ac4f9d5e98a48ddec6579ecc
Liam Sexton <liam@Liams-MacBook-Pro.local>
2018-07-23 13:19:30 -0700

    update_average method no longer returns

--- MovingAverage.py
+++ MovingAverage.py
@@ -8,9 +8,8 @@ class MovingAverage:
     # Updates the average of this moving average object
     def update_average(self, new_value):
         if len(self.data) == self.interval_len:
-            new_average = (self.average * len(self.data) + new_value - self.data[0]) / float(self.interval_len)
+            self.average = (self.average * len(self.data) + new_value - self.data[0]) / float(self.interval_len)
             self.data.pop(0)
         else:
-            new_average = (self.average * len(self.data) + new_value) / float((len(self.data) + 1))
+            self.average = (self.average * len(self.data) + new_value) / float((len(self.data) + 1))
         self.data.append(new_value)
-        return new_average

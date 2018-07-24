18b64ed29b3c970603603e38836ac9a174c36f64
Andre Huang <huangand000@gmail.com>
2018-07-24 15:16:41 -0700

    Added timer

--- StoveDetection.py
+++ StoveDetection.py
@@ -5,7 +5,7 @@ from MovingAverage import MovingAverage
 class SquelchAlgorithmClass:
     
     # assuming one data point (a temperature) taken every minute during interval length
-    def __init__(self, interval_len, threshold=29):
+    def __init__(self, interval_len):
         # A list used to capture a set length of data points
         self.data_points = [] 
         # A dynamic moving/running average of the data points
@@ -14,10 +14,8 @@ class SquelchAlgorithmClass:
         self.interval_len = interval_len
         # The state ON/OFF
         self.state = "OFF"
-        # A set temperature threshold, DEFAULT = 29 Celsius
-        self.threshold = threshold 
         # dictionary, can also be used for finding running average of total time
-        self.state_history = {}
+        self.state_history = []
         # Dictionary used to track the current states
         self.curr_states = {} 
         # The current maximum temperature
@@ -25,14 +23,18 @@ class SquelchAlgorithmClass:
         # Current temperature
         self.curr_temp = None
         self.delta = 3
+        # Timer for stove when turned on
+        self.timer = datetime.datetime.now()
 
 
-    # Need more checks: if no AC, house could be above threshold temp
     def update_moving_average(self, new_temp):
-        # self._initial_data_gathering(new_temp)
+        self._check_timer()
+        if len(self.data_points) == 0:
+            self.moving_average = new_temp
         curr_state = self.state
-        if new_temp > self.threshold and new_temp > 3 + self.moving_average: # if hot and large increase in temp
+        if new_temp > 3 + self.moving_average: # if hot and large increase in temp
             self.state = 'ON'
+            self._start_timer()
             self.curr_states["ON"] = str(datetime.datetime.now()).split('.')[0]
             if new_temp < self.curr_temp:
                 self.state = 'SIMMERING'
@@ -50,20 +52,20 @@ class SquelchAlgorithmClass:
             else:
                 self.moving_average = (self.moving_average * len(self.data_points) + new_temp) / (len(self.data_points) + 1)
             self.data_points.append(new_temp)
-        self._check_and_stove_duration(curr_state, new_temp)
+        self._check_and_store_duration(curr_state, new_temp)
         
     def toString(self):
             print "Data Points:", self.data_points 
             print "Moving Average:", self.moving_average
             print "Interval Length:", self.interval_len
             print "State:", self.state
-            print "Threshold:", self.threshold 
             print "Current States", self.curr_states 
             print "State History", self.state_history
             print "Max Temperature:", self.max_temp 
             print "Last record Temperature:", self.curr_temp
+            print "Timer:", self.timer
 
-    def _check_and_stove_duration(self, curr_state, new_temp):
+    def _check_and_store_duration(self, curr_state, new_temp):
         if (curr_state == "ON" and self.state == "OFF") or (curr_state == "SIMMERING" and self.state == "OFF"):
             if ("ON" in self.curr_states or "SIMMERING" in self.curr_states) and "OFF" in self.curr_states:
                 on_time = datetime.datetime.strptime(self.curr_states["ON"], "%Y-%m-%d %H:%M:%S")
@@ -73,22 +75,19 @@ class SquelchAlgorithmClass:
                 self.state_history["stove_duration"].append(on_time - off_time)
         self.curr_temp = new_temp
 
-    # Returns the moving average of how long the stove has been on
-    def _moving_average_time(self):
-        # total_time = 0
-        # num_times = 0
-        # for key in self.curr_states.keys():
-                # if self.curr_states[key] == 'ON':
-        return
+    def _start_timer(self):
+        self.timer = datetime.datetime.now()
+
+    def _check_timer(self):
+        print datetime.datetime.now() - self.timer
 
 if __name__ == "__main__":
     sq = SquelchAlgorithmClass(5)
-    for i in range(0,5):
+    for i in range(0,2):
         sq.update_moving_average(25)
 
-    sq.update_moving_average(46)
-    sq.update_moving_average(35)
-    sq.update_moving_average(25)
+    sq.update_moving_average(20)
+    sq.update_moving_average(30)
+
     sq.toString()
 
-    mv = MovingAverage(2,3)
